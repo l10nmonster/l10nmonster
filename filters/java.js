b0ea331ee5dc55b0cabe41c6419a5ad67007d79d
Diego Lagunas <diegone@gmail.com>
2021-12-03 22:23:06 -0800

    Fix java filter

--- filters/java.js
+++ filters/java.js
@@ -2,22 +2,46 @@ import { parseToEntries, stringifyFromEntries } from '@js.properties/properties'
 
 export class JavaPropertiesFilter {
     async parseResource({ resource }) {
-        const parsedResource = parseToEntries(resource);
-        const translationUnits = parsedResource.map(e => ({
-            sid: e.key,
-            str: e.element,
-        }));
+        const parsedResource = parseToEntries(resource, { sep: true, eol: true, all: true, original: true });
+        const translationUnits = [];
+        let previousComment;
+        for (const e of parsedResource) {
+            if (e.key) {
+                const tu = {
+                    sid: e.key,
+                    str: e.element,
+                };
+                if (previousComment) {
+                    tu.notes = previousComment;
+                    previousComment = null;
+                }
+                translationUnits.push(tu);
+            } else {
+                previousComment = e.original;
+            }
+        }
         return {
             translationUnits,
         };
     }
 
     async generateTranslatedResource({ resourceId, resource, lang, translator }) {
-        const parsedResource = parseToEntries(resource);
-        const translatedEntries = parsedResource.map(async e => ({
-            key: e.key,
-            element: await translator(resourceId, e.key, e.element),
-        }));
+        const parsedResource = parseToEntries(resource, { sep: true, eol: true, all: true, original: true });
+        const translations = await Promise.all(parsedResource.map(async e => e.key && translator(resourceId, e.key, e.element)));
+        const translatedEntries = parsedResource.map((e, i) => {
+            const { original, element, ...rest } = e;
+            if (e.key) {
+                return {
+                    ...rest,
+                    element: translations[i],
+                };
+            } else {
+                return {
+                    ...rest,
+                    original,
+                };
+            }
+        });
         return stringifyFromEntries(translatedEntries);
     }
 }
