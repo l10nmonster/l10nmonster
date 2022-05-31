8a0c8aac457cec3b1359067d8bca775e67a6010f
Diego Lagunas <diegone@gmail.com>
2022-05-31 00:31:53 -0700

    Improve naming convention of jobs

--- stores/jsonJobStore.js
+++ stores/jsonJobStore.js
@@ -22,7 +22,7 @@ export class JsonJobStore {
     }
 
     async getJobStatusByLangPair(sourceLang, targetLang) {
-        const files = globbySync(path.join(this.jobsBaseDir, '*', `${sourceLang}_${targetLang}_job_*.json`));
+        const files = globbySync(path.join(this.jobsBaseDir, '*', `*${sourceLang}_${targetLang}_job_*.json`));
         const statusMap = {};
         for (const file of files) {
             const entry = file.match(/job_(?<guid>[0-9A-Za-z_-]+)-(?<status>req|wip|done)\.json$/)?.groups;
@@ -53,7 +53,7 @@ export class JsonJobStore {
     async updateJob(jobResponse, jobRequest) {
         const updatedAt = (this.ctx.regression ? new Date('2022-05-29T00:00:00.000Z') : new Date()).toISOString();
         const langPath = this.#jobsDirForPair(jobResponse.sourceLang, jobResponse.targetLang);
-        const filename = `${jobResponse.sourceLang}_${jobResponse.targetLang}_job_${jobResponse.jobGuid}`;
+        const filename = `${jobResponse.translationProvider}_${jobResponse.sourceLang}_${jobResponse.targetLang}_job_${jobResponse.jobGuid}`;
         const jobPath = path.join(langPath, `${filename}-${jobResponse.status === 'done' ? 'done' : 'wip'}.json`);
         await writeFileSync(jobPath, JSON.stringify({ ...jobResponse, updatedAt }, null, '\t'), 'utf8');
         if (jobRequest) {
