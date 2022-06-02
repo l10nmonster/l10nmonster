bf6a5ba4614877676495dbd7301a33eb6eb6b60f
Diego Lagunas <diegone@gmail.com>
2022-06-01 23:20:19 -0700

    Relax compatibility check
    When v1 is present, ignore the placeholder name and only look at order and type (bx/ex/x).

--- normalizers/util.js
+++ normalizers/util.js
@@ -103,15 +103,16 @@ export function extractNormalizedPartsFromXmlV1(str, phMap) {
     return normalizedParts;
 }
 
+const minifyV1PH = v1ph => v1ph && v1ph.split('_').slice(0, -1).join('_');
 export function sourceAndTargetAreCompatible(nsrc, ntgt) {
     if (Boolean(nsrc) && Boolean(ntgt)) {
         !Array.isArray(nsrc) && (nsrc = [ nsrc ]);
         !Array.isArray(ntgt) && (ntgt = [ ntgt ]);
-        const v1PhMap = flattenNormalizedSourceV1(nsrc)[1];
+        const v1PhMap = Object.fromEntries(Object.entries(flattenNormalizedSourceV1(nsrc)[1]).map(([k, v]) => [minifyV1PH(k), v]));
         const valueMap = Object.fromEntries(Object.values(v1PhMap).map(e => [ e.v, true ]));
         for (const ph of ntgt) {
             if (typeof ph === 'object') {
-                if (!v1PhMap[ph.v1] && !valueMap[ph.v]) {
+                if (!v1PhMap[minifyV1PH(ph.v1)] && !valueMap[ph.v]) {
                     return false;
                 }
             }
