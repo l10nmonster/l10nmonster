0aa672f7946e1a6ebab6fd16af5654a63f91f6fc
Theresa Khoo <theresa.khoo@gmail.com>
2022-07-29 11:02:10 -0700

    Changes based on feedback.

--- src/normalizers/keywordProtector.js
+++ src/normalizers/keywordProtector.js
@@ -1,29 +1,22 @@
 import { regexMatchingDecoderMaker } from './regex.js';
 
 export function keywordProtector (keywordMap) {
-    const defaultEncoder = regexMatchingEncoderMaker(
-        'protectedStringsDecoder',
-        /(?<protector>protector:\w+)/g,
-        Object.fromEntries(Object.keys(keywordMap).map(kw => [ `protector:${kw}`, kw ]))
-    );
-
-    const mappedEncoder = regexMatchingEncoderMaker(
-        'protectedStringsMappedDecoder',
+    const encoder = regexMatchingEncoderMaker(
+        'keywordProtectorDecoder',
         /(?<protector>protector:\w+)/g,
         Object.fromEntries(Object.keys(keywordMap).map(kw => [ `protector:${kw}`, keywordMap[kw] ]))
     );
-
     const decoder = regexMatchingDecoderMaker(
-        'protectedStringsDecoder',
+        'keywordProtectorDecoder',
         new RegExp(`(?<protector>${Object.keys(keywordMap).join("|")||"NO_PROTECTED_STRINGS"})`, 'g'),
         (groups) => ({ t: 'x', v: `protector:${groups.protector}` })
     );
-    return [ decoder, defaultEncoder, mappedEncoder ]
+    return [ decoder, encoder ]
 }
 
 //Checks for values in the map based on the flags
 function findFlagValue (charMap, flags) {
-    const v = !charMap || typeof charMap === 'string'? charMap : Object.values(flags).find((v) => charMap[v]);
+    const v = !charMap || typeof charMap === 'string' || Object.keys(charMap).length === 0 ? charMap : Object.values(flags).find((v) => charMap[v]);
     return !charMap ? charMap : !charMap[v] || typeof charMap[v] === 'string' ? charMap[v] : findFlagValue (charMap[v], flags);
 }
  
--- tests/normalizers/keywordProtector.test.js
+++ tests/normalizers/keywordProtector.test.js
@@ -2,24 +2,21 @@ import { keywordProtector } from '../../src/normalizers/keywordProtector';
 
 describe('Keyword Protector tests', () => {
 
-    const [decoder, defaultEncoder, mappedEncoder ] = keywordProtector({Hopper: {ja: 'ホッペー', ko: '홉 따는 사람'}, 'Price Freeze': {}}); 
+    const [ decoder, encoder ] = keywordProtector({Hopper: {ja: 'ホッペー', ko: '홉 따는 사람'}, 'Price Freeze': {}}); 
 
     test('keyword with mapped values', async() => {
 
         expect(decoder([ { t: "s", v: "Hopper: Flights, Hotels & Cars"} ]))
             .toMatchObject([ { t: "x", v: "protector:Hopper"}, { t: "s",  v: ": Flights, Hotels & Cars" } ]);
 
-        expect(defaultEncoder("protector:Hopper", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
-            .toMatch('Hopper');
-
-        expect(mappedEncoder("protector:Hopper", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
+        expect(encoder("protector:Hopper", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
             .toMatch('ホッペー');
 
-        expect(mappedEncoder("protector:Hopper", { targetLang: 'ko', prj: 'l10n-adhoc-requests' }))
+        expect(encoder("protector:Hopper", { targetLang: 'ko', prj: 'l10n-adhoc-requests' }))
             .toMatch('홉 따는 사람');
 
-        expect(mappedEncoder("protector:Hopper", { targetLang: 'da-DK', prj: 'l10n-adhoc-requests' }))
-            .toMatch('protector:Hopper');
+        expect(encoder("protector:Hopper", { targetLang: 'da-DK', prj: 'l10n-adhoc-requests' }))
+            .toMatch('Hopper');
 
     });
     test('keyword without mapped values ', async() => {
@@ -27,14 +24,11 @@ describe('Keyword Protector tests', () => {
         expect(decoder([ { t: "s", v: "Price Freeze Deposit and Savings from the rental car price breakdown Freeze"} ]))
             .toMatchObject([ { t: "x", v: "protector:Price Freeze"}, { t: "s",  v: " Deposit and Savings from the rental car price breakdown Freeze" } ]);
 
-        expect(defaultEncoder("protector:Price Freeze", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
+        expect(encoder("protector:Price Freeze", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
             .toMatch('Price Freeze');
 
-        expect(mappedEncoder("protector:Price Freeze", { targetLang: 'ja', prj: 'l10n-adhoc-requests' }))
-            .toMatch('protector:Price Freeze');
-
-        expect(mappedEncoder("protector:Price Freeze", { targetLang: 'da-DK', prj: 'l10n-adhoc-requests' }))
-            .toMatch('protector:Price Freeze');
+        expect(encoder("protector:Price Freeze", { targetLang: 'da-DK', prj: 'l10n-adhoc-requests' }))
+            .toMatch('Price Freeze');
 
     });    
 
