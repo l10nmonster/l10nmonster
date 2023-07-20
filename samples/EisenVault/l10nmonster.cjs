a131af6b8e5e3df5863836c54f8d56812e17428a
Diego Lagunas <diegone@gmail.com>
2023-07-20 19:32:07 +0200

    Add sample linter as an extension command

--- samples/EisenVault/l10nmonster.cjs
+++ samples/EisenVault/l10nmonster.cjs
@@ -1,24 +1,23 @@
 const java = require('@l10nmonster/helpers-java');
 const { xml, stores, adapters, normalizers } = require('@l10nmonster/helpers');
+const lint = require('./lint.cjs');
 
 module.exports = class EisenVaultConfig2 {
     sourceLang = 'en';
     targetLangs = [ 'it', 'ja', 'pt-BR' ];
     minimumQuality = 50;
-
-    constructor() {
-        this.source = new adapters.FsSource({
-            globs: [ '**/*_en.properties' ],
-            resDecorator: resMeta => ({ ...resMeta, prj: resMeta.id.split('/')[1].split('.')[0].split('-')[0]}),
-        });
-        this.snapStore = new stores.FsSnapStore();
-        this.resourceFilter = new java.PropertiesFilter();
-        this.decoders = [ normalizers.bracePHDecoder, xml.tagDecoder, java.escapesDecoder ];
-        this.target = new adapters.FsTarget({
-            targetPath: (lang, resourceId) => resourceId.replace('_en.properties', `_${lang.replace('-', '_')}.properties`),
-        });
-        this.jobStore = new stores.JsonJobStore({
-            jobsDir: 'l10njobs',
-        });
-    }
+    source = new adapters.FsSource({
+        globs: [ '**/*_en.properties' ],
+        resDecorator: resMeta => ({ ...resMeta, prj: resMeta.id.split('/')[1].split('.')[0].split('-')[0]}),
+    });
+    snapStore = new stores.FsSnapStore();
+    resourceFilter = new java.PropertiesFilter();
+    decoders = [ normalizers.bracePHDecoder, xml.tagDecoder, java.escapesDecoder ];
+    target = new adapters.FsTarget({
+        targetPath: (lang, resourceId) => resourceId.replace('_en.properties', `_${lang.replace('-', '_')}.properties`),
+    });
+    jobStore = new stores.JsonJobStore({
+        jobsDir: 'l10njobs',
+    });
+    static extensionCmds = [ lint ];
 }
