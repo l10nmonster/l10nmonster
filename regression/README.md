3fa17d58a961158f9e11dcb13a14cb6e71302271
Diego Lagunas <diegone@gmail.com>
2024-12-02 11:14:42 -0800

    Improve regression testing
    Can now customize what scripts to run per regression case.
    Command line can specify which case to run.
    Fixed cases that were failing.
    Enabled regression test in GH workflow.

--- regression/README.md
+++ regression/README.md
@@ -1,6 +1,20 @@
 # Regression Tests
 
 This suite of tests should be used for testing that code changes don't introduce regressions.
-It can be run from the shell with the `zsh test.zsh` command passing 2 parameters:
-1. The first parameter controls whether the test suite is run by the command line or via the JS API. The values are respectively `cli` and `js`.
+It can be run from the shell with the `test.zsh` command passing 3 parameters:
+1. The first parameter controls whether the test suite is run from the command line (the `regressionScript.zsh` script) or via the JS API (the `regressionScript.cjs` script). The values are respectively `cli` and `js`.
 2. The second parameter controls whether the code to test is the locally modified one or the latest one on npm. The values are respectively `local` and `npm`. Please note that in order to run locally, each local package must have its dependencies installed. You can do that by running `find . -maxdepth 2 -name package.json -execdir npm i \;` from the base of the project.
+3. The third parameter specifies which regression test to run. Passing `all` runs all tests.
+
+## Overview
+
+Each regression case usually tests a different kind of setup and potentially runs different commands but ultimately they function in the same way:
+1. An initial `mint` set of files is copied into a `wd` working directory.
+2. A series of l10n monster operations is run.
+3. The final wd directory is compared against an `expected` directory, where all files must match.
+
+## How to add a new regression case
+
+1. Add a new folder with the `mint` and `expected` directories.
+2. Make sure the `mint` folder contains `regressionScript.cjs` and `regressionScript.zsh` scripts.
+3. If needed, add new dependencies to `package-local.json` and `package-npm.json`.
--- regression/test.zsh
+++ regression/test.zsh
@@ -1,20 +1,21 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <cli|js> <local|npm>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <cli|js> <local|npm> <all|regression_case>"
     exit 1
 fi
 
 setopt ERR_EXIT
-#setopt MONITOR
 
+find expected -name '.DS_Store' -type f -delete
 rm -rf wd
-# rm **/.DS_Store
-mkdir wd
-cp -pr mint/* wd
-# cp -pr mint/gramps wd
-cp regressionScript.cjs wd
+mkdir -p wd/cases
 
+if [[ "$3" == "all" ]]; then
+    cp -pr mint/* wd/cases
+else
+    cp -pr mint/$3 wd/cases
+fi
 
 if [[ "$2" == "npm" ]]; then
     cp package-npm.json wd/package.json
@@ -27,31 +28,25 @@ echo "Installing npm dependencies..."
 npm i --no-package-lock
 npm ls
 
+cd cases
 for dir in *
 do
-    if [[ "$dir" != "node_modules" &&  "$dir" != "package.json" &&  "$dir" != "regressionScript.cjs" ]]; then
-        echo "\nTesting $dir..."
-        cd $dir
-        if [[ "$1" == "cli" ]]; then
-            time ../node_modules/@l10nmonster/cli/l10n.cjs --regression -v2 push --provider grandfather,repetition,default
-            time ../node_modules/@l10nmonster/cli/l10n.cjs --regression -v2 pull
-            time ../node_modules/@l10nmonster/cli/l10n.cjs --regression -v2 translate all
-            time ../node_modules/@l10nmonster/cli/l10n.cjs --regression -v2 status --output status.json
-            time ../node_modules/@l10nmonster/cli/l10n.cjs --regression -v2 tmexport
-        else
-            time node ../regressionScript.cjs
-        fi
-        cd ..
+    echo "\nTesting $dir..."
+    cd $dir
+    if [[ "$1" == "cli" ]]; then
+        time zsh regressionScript.zsh
+    else
+        time node regressionScript.cjs
     fi
+
+    echo "\nDiffing working dir vs. expected..."
+    rm -rf .l10nmonster
+    rm regressionScript.cjs
+    rm regressionScript.zsh
+    rm l10nmonster.cjs
+    cd ..
+    find $dir -name '.DS_Store' -type f -delete
+    diff -qr $dir ../../expected/$dir
 done
-cd ..
-wait
-echo "\nDiffing working dir vs. expected..."
-rm -rf wd/*/.l10nmonster
-rm -rf wd/node_modules
-rm wd/regressionScript.cjs
-rm wd/package.json
-rm wd/*/l10nmonster.cjs
-find wd -name '.DS_Store' -type f -delete
-find expected -name '.DS_Store' -type f -delete
-diff -qr wd expected
+
+echo "Regression test completed successfully (error code $?)"
