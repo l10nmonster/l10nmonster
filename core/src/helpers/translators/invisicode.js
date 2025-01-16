c7d44ddb2140dfb9ad9d8d44ae4184630cfbee7a
Diego Lagunas <diegone@gmail.com>
2025-01-16 12:15:52 -0800

    Add invisicode glasses script

--- core/src/helpers/translators/invisicode.js
+++ core/src/helpers/translators/invisicode.js
@@ -21,29 +21,6 @@ function utf8ToFE00Range(input) {
   return result.join(''); // Join array into a single string
 }
 
-// const invisicodeRegex = /\u200B([\uFE00-\uFE0F]+)([^\u200B]*?)\u200B/g;
-// const decoder = new TextDecoder();
-
-// function fe00RangeToUtf8(encoded) {
-//   const length = encoded.length;
-
-//   if (length % 2 !== 0) {
-//     throw new Error("Invalid encoded input length");
-//   }
-
-//   const bytes = new Uint8Array(length / 2);
-//   let byteIndex = 0;
-
-//   for (let i = 0; i < length; i += 2) {
-//     const highNibble = encoded.charCodeAt(i) - base;
-//     const lowNibble = encoded.charCodeAt(i + 1) - base;
-//     bytes[byteIndex++] = (highNibble << 4) | lowNibble;
-//   }
-
-//   return decoder.decode(bytes);
-// }
-
-
 /**
  * @class InvisicodeGenerator
  * @extends {import('@l10nmonster/core').Translator}
