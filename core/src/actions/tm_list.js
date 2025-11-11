e1b4742ed616202efcbb9f175562bae872a6ab61
Diego Lagunas <diegone@gmail.com>
2025-11-11 11:17:04 -0800

    fix(core): Improve tm list presentation and defaults

--- core/src/actions/tm_list.js
+++ core/src/actions/tm_list.js
@@ -24,18 +24,22 @@ export class tm_list {
                 consoleLog`\nNothing in the local TM Cache`;
             } else {
                 consoleLog`\nLocal TM Cache:`;
-                const pctFormatter = new Intl.NumberFormat('en-US', {
-                    style: 'percent',
-                    minimumFractionDigits: 0,
-                    maximumFractionDigits: 0,
-                });
-                for (const [ srcLang, tgtLang ] of pairs) {
-                    consoleLog`  ‣ ${srcLang} → ${tgtLang}`;
-                    const tm = await monsterManager.tmm.getTM(srcLang, tgtLang);
-                    const tmStats = await tm.getStats();
-                    for (const stats of tmStats) {
-                        consoleLog`      • ${stats.translationProvider}(${stats.status}): ${stats.jobCount.toLocaleString()} ${[stats.jobCount, 'job', 'jobs']}, ${stats.tuCount.toLocaleString()} ${[stats.tuCount, 'tu', 'tus']}, ${stats.distinctGuids.toLocaleString()} ${[stats.distinctGuids, 'guid', 'guids']} ${pctFormatter.format(stats.tuCount / stats.distinctGuids - 1)} redundancy`;
+                if (options.detailed) {
+                    const pctFormatter = new Intl.NumberFormat('en-US', {
+                        style: 'percent',
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 0,
+                    });
+                    for (const [ srcLang, tgtLang ] of pairs) {
+                        consoleLog`  ‣ ${srcLang} → ${tgtLang}`;
+                        const tm = await monsterManager.tmm.getTM(srcLang, tgtLang);
+                        const tmStats = await tm.getStats();
+                        for (const stats of tmStats) {
+                            consoleLog`      • ${stats.translationProvider}(${stats.status}): ${stats.jobCount.toLocaleString()} ${[stats.jobCount, 'job', 'jobs']}, ${stats.tuCount.toLocaleString()} ${[stats.tuCount, 'tu', 'tus']}, ${stats.distinctGuids.toLocaleString()} ${[stats.distinctGuids, 'guid', 'guids']} ${pctFormatter.format(stats.tuCount / stats.distinctGuids - 1)} redundancy`;
+                        }
                     }
+                } else {
+                    consoleLog`  ‣ ${pairs.map(([ srcLang, tgtLang ]) => `${srcLang} → ${tgtLang}`).join(', ')}`;
                 }
             }
         }
@@ -46,15 +50,15 @@ export class tm_list {
             for (const tmStoreId of tmStoreIds) {
                 const tmStore = await monsterManager.tmm.getTmStore(tmStoreId);
                 consoleLog`  ‣ ${tmStoreId}: ${tmStore.constructor.name} access: ${tmStore.access} partitioning: ${tmStore.partitioning}`;
-                const tocs = await monsterManager.tmm.getTmStoreTOCs(tmStore, options.parallelism);
-                for (const [ srcLang, tgtLang, toc ] of tocs) {
-                    const blocks = Object.values(toc.blocks);
-                    const jobs = blocks.reduce((acc, block) => acc + block.jobs.length, 0);
-                    consoleLog`      • ${srcLang} → ${tgtLang}: ${blocks.length.toLocaleString()} ${[blocks.length, 'block', 'blocks']} ${jobs.toLocaleString()} ${[jobs, 'job', 'jobs']}`;
-                    if (options.detailed) {
-                        for (const block of blocks) {
-                            consoleLog`        • ${block.blockName}. Jobs: ${block.jobs.map(e => e[0]).join(', ')}`;
-                        }
+                if (options.detailed) {
+                    const tocs = await monsterManager.tmm.getTmStoreTOCs(tmStore, options.parallelism);
+                    for (const [ srcLang, tgtLang, toc ] of tocs) {
+                        const blocks = Object.values(toc.blocks);
+                        const jobs = blocks.reduce((acc, block) => acc + block.jobs.length, 0);
+                        consoleLog`      • ${srcLang} → ${tgtLang}: ${blocks.length.toLocaleString()} ${[blocks.length, 'block', 'blocks']} ${jobs.toLocaleString()} ${[jobs, 'job', 'jobs']}`;
+                        // for (const block of blocks) {
+                        //     consoleLog`        • ${block.blockName}. Jobs: ${block.jobs.map(e => e[0]).join(', ')}`;
+                        // }
                     }
                 }
             }
