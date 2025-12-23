32b103b12cf2913d25b587e124a93b58d4771b66
Diego Lagunas <diegone@gmail.com>
2025-12-22 19:15:09 -0800

    chore(release): Fix release script [skip ci]

--- core/types/index.d.ts
+++ core/types/index.d.ts
@@ -4687,7 +4687,7 @@ export function requiredSourcePluralForms(lang?: string): string[];
  */
 export function requiredTargetPluralForms(langs?: string[]): string[];
 export const l10nMonsterPackage: "@l10nmonster/core";
-export const l10nMonsterVersion: "3.1.0";
+export const l10nMonsterVersion: "3.1.1";
 export const l10nMonsterDescription: "L10n Monster Core Module";
 type Segment$2 = Segment;
 type Part$6 = Part;
